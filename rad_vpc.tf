3f0ffb17f8d286449a944c62e99b16d367240804
jflab1 <jfempire@yahoo.co.uk>
2019-03-14 12:58:24 +0530

    vpc only

---
+++ rad_vpc.tf
@@ -0,0 +1,46 @@
+//Providers
+provider "aws" {
+  access_key  = "${var.aws_access_key}"
+  secret_key  = "${var.aws_secret_key}"
+  region      = "${var.region}"
+  version     = "~> 1.5"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+//VPC
+resource "aws_vpc" "vpc_rad" {
+    assign_generated_ipv6_cidr_block  = "false"
+    enable_dns_hostnames              = "true"
+    enable_dns_support                = "true"
+    cidr_block                        = "${var.cidr_ipv4}"
+    tags                              = {Name = "vpc_rad"}
+}
+
+//Public Subnet
+resource "aws_subnet" "sn_public" {
+  vpc_id                  = "${aws_vpc.vpc_rad.id}"
+  cidr_block              = "${var.public_subnet}"
+  #Hardcode Availability Zone to avoid t2.micro from getting created in 2d
+  #availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = "true"
+  tags                    = {    Name = "sn_rad_public"  }
+}
+resource "aws_route_table" "rt_rad_public" {
+  vpc_id  = "${aws_vpc.vpc_rad.id}"
+  tags    = {Name = "rt_rad_public"}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig_rad.id}"
+  }
+}
+resource "aws_route_table_association" "rta_rad_public" {
+  subnet_id      = "${aws_subnet.sn_public.id}"
+  route_table_id = "${aws_route_table.rt_rad_public.id}"
+}
+resource "aws_internet_gateway" "ig_rad" {
+  vpc_id  = "${aws_vpc.vpc_rad.id}"
+  tags    = {Name="ig_rad"}
+}
---
+++ tf-variable-definitions.tf
@@ -0,0 +1,45 @@
+variable "ec2_ami" {
+  default=       "ami-0385455dc2b1498ef"     
+}
+variable "nat_ami" {
+  default=       "ami-0b840e8a1ce4cdf15"     
+}
+variable "aws_access_key" {
+  default     = ""
+  description = "Amazon AWS Access Key"
+}
+variable "aws_secret_key" {
+  default     = ""
+  description = "Amazon AWS Secret Key"
+}
+variable "region" {
+  default     = "us-west-2"
+  description = "Amazon AWS Region for deployment"
+}
+
+variable "type" {
+  default     = "t2.micro"
+  description = "Amazon AWS Instance Type"
+}
+variable "ssh_key_name" {
+  default     = ""
+  description = "Amazon AWS Key Pair Name"
+}
+variable "ssh_key_file_name" {
+  default     = ".pem"
+  description = "Amazon AWS Key Pair Name with the extension of the actual file name"
+}
+
+variable "cidr_ipv4" {
+  description = "CIDR for IPV4"
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet" {
+  description = "Public Subnet CIDR"
+  default = "10.0.0.0/24"
+}
+variable "private_subnet" {
+  description = "Private Subnet CIDR"
+  default = "10.0.1.0/24"
+}
